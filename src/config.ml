33fd4d68c8aa3626aeae7426b06584b9f3ba887c
Hezekiah M. Carty <hez@0ok.org>
2012-09-05 23:11:06 -0400

    Update config.ml to reflect the forced-everything status

--- src/config.ml
+++ src/config.ml
@@ -14,16 +14,16 @@
 
 (* $Id: camlimages.ml.in,v 1.3.2.1 2010/05/13 13:14:47 furuse Exp $ *)
 
-let version = "4.0.2";;
+let version = "0.1";;
 
 (* Supported libraries *)
 let lib_gif = true;;
 let lib_png = true;;
 let lib_jpeg = true;;
 let lib_tiff = true;;
-let lib_freetype = false;;
+let lib_freetype = true;;
 let lib_ps = true;;
-let lib_xpm = false;;
+let lib_xpm = true;;
 
 (* External files *)
 let path_rgb_txt = "/dev/null";;
