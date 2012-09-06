8aa8d989a3e4f93c66b4e55fd87a4328a62670f6
Hezekiah M. Carty <hez@0ok.org>
2012-09-06 10:25:49 -0400

    Generate config.ml's content based on oasis's configuration results

---
+++ src/config.mli
@@ -0,0 +1,14 @@
+val version : string
+val lib_gif : bool
+val lib_png : bool
+val lib_jpeg : bool
+val lib_tiff : bool
+val lib_freetype : bool
+val lib_ps : bool
+val lib_xpm : bool
+val path_rgb_txt : string ref
+val path_gs : string ref
+val lib_ppm : bool
+val lib_bmp : bool
+val lib_xvthumb : bool
+val word_size : int
